a593bfb4bd3fd55991516d45f4f6ee540a647052
ramonbarbosdev <ramonplay2016@gmail.com>
2025-01-22 07:58:50 -0300

    passando parametros

--- src/main/java/br/com/curso/controller/IndexController.java
+++ src/main/java/br/com/curso/controller/IndexController.java
@@ -4,16 +4,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController /*ARQUITETURA REST*/
 @RequestMapping(value = "/usuario")
 public class IndexController {
 	
+	/*SERVIÇO RESTFULL*/
 	@GetMapping(value = "/", produces = "application/json")
-	public ResponseEntity init()
+	public ResponseEntity init(@RequestParam (value = "nome", required = false, defaultValue = "Nome não informado") String nome, @RequestParam(value="salario", required = false) Long salario  )
 	{
-		return new ResponseEntity<>("Olá Mundo, usuario", HttpStatus.OK);
+		
+		return new ResponseEntity<>("Usuario: " + nome + ", Salario: "+ salario, HttpStatus.OK);
 	}
 
 }
