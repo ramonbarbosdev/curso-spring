231d7c3dfa99405f0cd1c65f8af2aa77391f1802
ramonbarbosdev <ramonplay2016@gmail.com>
2025-01-23 13:23:58 -0300

    Criando nosso serviço POST e efetuando um cadastro de usuário

--- src/main/java/br/com/curso/controller/IndexController.java
+++ src/main/java/br/com/curso/controller/IndexController.java
@@ -9,6 +9,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +26,7 @@ public class IndexController {
 	private UsuarioRepository usuarioRepository;
 	
 	/*SERVIÇO RESTFULL*/
+	
 	@GetMapping(value = "/{id}", produces = "application/json")
 	public ResponseEntity<Usuario> init(@PathVariable (value="id") Long id )
 	{
@@ -39,6 +42,14 @@ public class IndexController {
 		List<Usuario> list = (List<Usuario>) usuarioRepository.findAll();
 		
 		return new ResponseEntity<List<Usuario>>(list, HttpStatus.OK);
+	}
+	
+	@PostMapping(value="/" , produces = "application/json")
+	public ResponseEntity<Usuario> cadastrar(@RequestBody Usuario usuario)
+	{
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 	}
 	
 	/*
