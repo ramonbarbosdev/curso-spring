691e273368e23ca2e0ec316da0669dd2939a9a59
ramonbarbosdev <ramonplay2016@gmail.com>
2025-01-23 13:39:58 -0300

    Criando a atualização de cadastro com o PUT

--- src/main/java/br/com/curso/controller/IndexController.java
+++ src/main/java/br/com/curso/controller/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -51,6 +52,17 @@ public class IndexController {
 		
 		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
 	}
+	
+	@PutMapping(value="/" , produces = "application/json")
+	public ResponseEntity<Usuario> atualizar(@RequestBody Usuario usuario)
+	{
+		/*outras rotinas..*/
+		
+		Usuario usuarioSalvo = usuarioRepository.save(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioSalvo, HttpStatus.OK);
+	}
+	
 	
 	/*
 	@GetMapping(value = "/", produces = "application/json")
